8c8625ecef8b4bc6d7a78fbd8145be39f9f1c59f
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 12:40:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,8 @@
-reverse_each_word(sentence1)
+def reverse_each_word(sentence1)
+  array = sentence1.split("")
+  array2 = []
+  array.collect do |rev_word|
+    array2 << "#{rev_word.reverse}"
+  end
+  array2.join("")
+end
